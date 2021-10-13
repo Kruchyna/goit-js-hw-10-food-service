881d296a8cccfd72d76034776bc344328d6463ec
Kruchyna Elena <kruchynaelena@gmail.com>
2021-10-14 00:00:27 +0300

    files project

---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+export default {
+    body: document.querySelector('body'),
+    switchControl: document.querySelector('.theme-switch__control'),
+    switchToggle: document.getElementById('theme-switch-toggle'),
+    listMenu: document.querySelector('.js-menu')
+}
+
